83d1dc59818a548b9ae27c533a8a928a17d877e7
RivrSantos <141034889+RivrSantos@users.noreply.github.com>
2025-01-03 17:57:58 -0300

    Add files via upload
    Houve pequenas mudanças em relaçao aos dados e a logo

--- teste_map.py
+++ teste_map.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import streamlit as st
 import pydeck as pdk
+import time
 
-st.logo("inmotors.png")
-tab_moto= pd.read_csv("cordenadas.csv")
-tabg= pd.read_csv("genero.csv")
-tot= tabg.median()
+st.sidebar.image("Black.png")
+tab_moto= pd.read_csv("C:/Users/user/Desktop/WORKING_SQL/Relatorios/cordenadas.csv")
+tabg= pd.read_csv("C:/Users/user/Desktop/WORKING_SQL/genero.csv")
+# cont= st.container(border= True, height= 60)
+    
 def maps():
+    st.sidebar.metric(label="TOTAL DE ATENDIMENTOS", value=76,
+                      delta='2.1%', delta_color='normal')
     df= pd.DataFrame(tab_moto)
-    container_map = st.container(border=True, height=720)
-    container_map.subheader('RELATÓRIO - DISPERSÃO ', divider=True)
+    container_map = st.container(border=True, height=680)
+    container_map.subheader('ANALISE - DISPERSAO DEMOGRAFICA', divider=True)
     container_map.pydeck_chart(pdk.Deck(
         initial_view_state=pdk.ViewState(
             latitude=-16.71824512937061,
@@ -33,24 +37,34 @@ def maps():
             ),
         ],
     ))
+    st.sidebar.subheader("GENERO", divider= True)
+    gen= st.sidebar.radio(
+        "",
+        ["Homens", "Mulheres"],
+        index= None
+    )
+
+    if gen == "Homens":
+        with st.spinner('carregando'):
+            time.sleep(2)
+            st.sidebar.subheader(". 83,9%")
+            st.sidebar.markdown(". Media cc: 250")
+            st.sidebar.markdown(". Ticket: R$ 28.042,13")
+            st.sidebar.divider()
+    elif gen == "Mulheres":
+        with st.spinner('carregando'):
+            time.sleep(2)
+            st.sidebar.subheader(". 16,1%")
+            st.sidebar.markdown(". Media cc: 150")
+            st.sidebar.markdown(". Ticket: R$ 17.212,24")
+            st.sidebar.divider()
+    else:
+        st.sidebar.divider()
 
     container_map.info('🌎 . Os dados são de fonte local, demonstrando a dispersão geográfica de clientes em Montes Claros e região')
-
-with st.popover("ANÁLISE - GEOGRÁFICA"):
-    st.markdown("A análise de dispersão geográfica é uma ferramenta poderosa para otimizar campanhas de publicidade, permitindo direcionar recursos, personalizar mensagens e maximizar o retorno sobre o investimento. Ao compreender como o nosso público está distribuído no espaço, podemos tomar decisões mais estratégicas para alcançar melhores resultados.")
-st.sidebar.metric(label="TOTAL DE ATENDIMENTOS", value=76,
-                  delta='2.1%', delta_color='normal')
-
 maps()
-gen= st.sidebar.radio(
-    "GENERO",
-    ["Homens", "Mulheres"],
-    index= None
-)
-if gen == "Homens":
-    st.sidebar.write("83,9%")
-elif gen == "Mulheres":
-    st.sidebar.write("16,1%")
 
-st.sidebar.subheader("CORDENADAS")
-st.sidebar.dataframe(tab_moto, hide_index= True)
+
+
+# st.sidebar.subheader("CORDENADAS")
+# st.sidebar.dataframe(tab_moto, hide_index= True)
